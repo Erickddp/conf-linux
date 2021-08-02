cfa75fa6a25d0885612f2fd937f6b443be051d0e
Erickddp <82197152+Erickddp@users.noreply.github.com>
2021-08-02 14:56:28 -0500

    Add files via upload

---
+++ inicio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Trabajando en ello..............."
+
+apt update
+
+apt upgrade -y
+
+setxkbmap es sundeadkeys
+
+echo "Listo!!! Tu sistema esta actualizado y configurado para con el teclado al español"
+
